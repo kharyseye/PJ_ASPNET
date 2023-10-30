7d639c63a2596ff95dd0765e826cefc2cf7b58ad
kharyseye <mamekharyseye380@gmail.com>
2023-10-30 16:12:12 +0000

    first commit

---
+++ BoutiqueDITI5/Model/Article.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoutiqueDITI5.Model
+{
+    public class Article
+    {
+        [Key]
+        public int IdArticle { get; set; }
+        [Required]
+        public int CodeArticle { get; set; }
+        public string Description { get; set; } 
+        public int Quantite { get; set; }
+
+        public double PrixUnit { get;set; }
+
+        public DateTime DatePeremption { get; set; }
+
+    }
+}
